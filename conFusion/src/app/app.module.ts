259749b830a5c6acdbfce30c1b603b43cbc840d4
Hongsang Yoo <kebinono@gmail.com>
2017-10-24 04:07:16 +1100

    Single Page Applications Part1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,7 @@ import { DishService } from './services/dish.service';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

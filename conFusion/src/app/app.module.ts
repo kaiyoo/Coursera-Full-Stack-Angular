40b4dccb2c4f0e1b1166af9dc697ef0943b01601
Hongsang Yoo <kebinono@gmail.com>
2017-11-02 01:15:34 +1100

    Template Forms Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -18,10 +18,12 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+
 
 @NgModule({
   declarations: [
@@ -32,7 +34,11 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
+  ],
+  entryComponents: [
+        LoginComponent
   ],
   imports: [
     BrowserModule,
